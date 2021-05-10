839a7e14c6cdce10f79692ca01b5240abfa07334
Yaswitha-MU <73365776+Yaswitha-MU@users.noreply.github.com>
2021-05-10 13:04:07 -0500

    Update dummies.py

--- source_code/Data_processing/dummies.py
+++ source_code/Data_processing/dummies.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 25 08:58:52 2020
-
-@author: USER
+Created on Tue Oct 20 17:13:55 2020
+Last Modified Date - 18/02/2021
+@author: Yaswitha Jampani
+Purpose: This script is to create the dummy varaibles 
+Source: SEER_final_dataset.csv
 """
-import pandas as pd 
-import matplotlib.pyplot as plt
 
-df= pd.read_csv(r"C:\Users\USER\Desktop\seer_Final_dataset.csv")
+
+import pandas as pd 
+
+#loading the csv file
+df= pd.read_csv(r"file_path.csv")
 df2=pd.get_dummies(df,columns=['Marital status at diagnosis','Race recode (White, Black, Other)','Sex','Primary Site','Grade','Laterality','Diagnostic Confirmation','Behavior code ICD-O-3','Radiation sequence with surgery','Reason no cancer-directed surgery','Radiation recode','Chemotherapy recode (yes, no/unk)','First malignant primary indicator','Histology'])
 df=pd.concat([df2,df['Survival_status']],axis=1)
-df.to_csv(r"C:\Users\USER\Desktop\seer_Final_dataset_dummy.csv")
+df.to_csv(r"file_path.csv")
